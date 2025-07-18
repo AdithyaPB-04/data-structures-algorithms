10c5304c1c2644b2d2d9c18c7982da9019760f5b
Adithya P B <adithya.binu@appsteamtechnologies.com>
2025-07-18 14:30:22 +0530

    added one more logic in missing number calculation

--- loop/missing-number.js
+++ loop/missing-number.js
@@ -2,7 +2,21 @@
 Given an aaray nums containing n distinct numbers in the
 range [0,n], return the only number in the range tht is missing from the array
 */ 
-function missingNumber(num){
+
+function missingNumber1(num){
+    let sumOfArray = 0;
+    let sumOfRange = 0;
+    for(let i = 0 ; i<num.length;i++){
+        sumOfArray+=sumOfArray[i]
+    }
+    for(let i=0; i<num.length;i++){         // or use n*(n-1)/2 equation
+        sumOfRange+=1
+    }
+    return sumOfRange-sumOfArray;
+}
+console.log(missingNumber2([0,1,2,3,5,6,7,8,9,10]))
+
+function missingNumber2(num){
     for(let i = 0 ; i<=num.length;i++){
         let found = false;
         for(let j = 0;j<num.length;j++){
@@ -16,4 +30,4 @@ function missingNumber(num){
         }
     }
 }
-console.log(missingNumber([0,1,2,4,5,6,7,8,9,10]))
+console.log(missingNumber2([0,1,2,4,5,6,7,8,9,10]))
