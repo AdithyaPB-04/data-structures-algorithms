ce81acc3cbaacfdc65a4eeec938554fa6a09ae2f
Adithya P B <adithya.binu@appsteamtechnologies.com>
2025-09-17 17:37:01 +0530

    string

---
+++ string/basic.js
@@ -0,0 +1,19 @@
+//String in Javascript
+
+let firstName = "Adithya";
+
+//length of a string
+console.log(firstName.length);
+
+//Access string elements
+console.log(firstName.charAt(1))
+console.log(firstName[1])
+console.log(firstName.charCodeAt(1)) //100 this is a ASCII value
+
+//to check whether the string present present in or not
+console.log(firstName.includes('i'))
+console.log(firstName.includes('adi'))
+
+console.log(firstName.indexOf('i'))   //2
+console.log(firstName.indexOf('adi')) //-1
+
