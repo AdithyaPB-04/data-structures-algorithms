15f9137357cf894913f7cac243a15c615b129cd2
Adithya P B <adithya.binu@appsteamtechnologies.com>
2025-09-19 17:28:55 +0530

    basics of string

--- string/basic.js
+++ string/basic.js
@@ -1,6 +1,6 @@
 //String in Javascript
 
-let firstName = "Adithya";
+let firstName = "Adithyai";
 
 //length of a string
 console.log(firstName.length);
@@ -15,5 +15,32 @@ console.log(firstName.includes('i'))
 console.log(firstName.includes('adi'))
 
 console.log(firstName.indexOf('i'))   //2
-console.log(firstName.indexOf('adi')) //-1
+console.log(firstName.indexOf('di')) //1
+
+//internal working of indexOf
+//for single element
+
+function findIndex(str,element){
+    for(let i=0;i<str.length;i++){
+        if(str[i]==element){
+            return i
+        }
+    }
+    return -1
+}
+console.log(findIndex("Adithya","i"))
+
+console.log(firstName.lastIndexOf("i"))
+
+let anotherName = "Adithya"
+console.log(firstName.localeCompare(anotherName))
+console.log(anotherName.localeCompare(firstName))
+
+//replace "Adithya" with "Sooraj"
+const str = "Adithya is Best Backend Developer, Adithya is the best"
+console.log(str.replace("Adithya","Sooraj"))
+console.log(str.replace("fgfggfe","Sooraj"))
+
+//to replace all 
+console.log(str.replaceAll("Adithya","Sooraj"))
 
