624b12e398490900987447c9c1ae1ec98d450d64
Adithya P B <adithya.binu@appsteamtechnologies.com>
2025-09-30 17:37:35 +0530

    string

--- string/basic.js
+++ string/basic.js
@@ -44,3 +44,43 @@ console.log(str.replace("fgfggfe","Sooraj"))
 //to replace all 
 console.log(str.replaceAll("Adithya","Sooraj"))
 
+console.log(str.replace(/Adithya/g,"Sooraj"))
+
+console.log(str.replace(/Adithya/gi,"Sooraj"))     //case sensitive
+
+/*
+Use replaceAll("text", "newText") for simple word/phrase replacements.
+Use replace(/pattern/gi, "newText") when you need case-insensitivity or patterns.
+*/
+
+console.log(str.substring(6))
+
+//substring of a string
+console.log(str.substring(6,20))
+console.log(str.slice(6,20))
+//slice and substring are almost same but one difference is we can write negative number in slice
+console.log(str.slice(-10,-2))
+
+console.log(str.search("Adithya")) //if present result will be 0 if absent result will be -1
+
+console.log(str.split(",")) //it splites into an array of string
+console.log(str.split(" ")) 
+//to split the character 
+console.log(str.split("")) 
+
+const subString = str.split(" ")
+console.log(subString.join(" "))
+
+console.log(str.startsWith("Adithya")) //if exists then true if not false
+console.log(str.endsWith("Adithya"))
+
+let whiteSpaceString = "                             Adithya is Best Backend Developer,                Adithya is the best"
+//to remove space
+
+console.log(whiteSpaceString.trim()) // it remove only extra white space itstarting and ending if there is any white space in between it will not trim
+
+//to remove from start only
+console.log(whiteSpaceString.trimStart())
+
+//to remove only end
+console.log(whiteSpaceString.trimEnd())
